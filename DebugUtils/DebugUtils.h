82f7fc8c123dd5b6385066644a84f1ee2974a1d2
Juan Pedro <JuanP.95.torres@gmail.com>
2017-12-26 23:18:33 +0100

    Change to 3 modes. Working.
    Mode 0: No debug
    Mode 1: Simple prinln
    Mode 2: Verbose debug

--- DebugUtils/DebugUtils.h
+++ DebugUtils/DebugUtils.h
@@ -2,21 +2,32 @@
 #define DEBUGUTILS_H
 
 
-#ifdef DEBUG
-#define DEBUG_PRINT(str)    \
-   Serial.print(millis());     \
-   Serial.print(": ");    \
-   Serial.print(__PRETTY_FUNCTION__); \
-   Serial.print(' ');      \
-   Serial.print(__FILE__);     \
-   Serial.print(':');      \
-   Serial.print(__LINE__);     \
-   Serial.print(' ');
+#if DEBUG==1 // SIMPLE DEBUG
+    #define DEBUG_PRINT(str)    Serial.print(str);
+    #define DEBUG_PRINTLN(str)  Serial.println(str);
+    #define DEBUG_VERBOSE(str)  \
+        Serial.print("Not showing verbose mode in line: "); \
+        Serial.print(__LINE__);             \
+        Serial.println(" due to not verbose configure.");
 
-#define DEBUG_PRINTLN(str) Serial.println(str);
-#else
-#define DEBUG_PRINT(str)
-#define DEBUG_PRINTLN(str) 
+#elif DEBUG == 2 // VERBOSE
+    #define DEBUG_PRINT(str)    DEBUG_VERBOSE(str);
+    #define DEBUG_PRINTLN(str)  DEBUG_VERBOSE(str);
+    #define DEBUG_VERBOSE(str)  \
+        Serial.print(millis()); \
+        Serial.print(": ");    \
+        Serial.print(__PRETTY_FUNCTION__); \
+        Serial.print(' ');      \
+        Serial.print(__FILE__);     \
+        Serial.print(':');      \
+        Serial.print(__LINE__);     \
+        Serial.print(' ');          \
+        Serial.println(str);
+
+#elif DEBUG == 0
+    #define DEBUG_PRINT(str)
+    #define DEBUG_PRINTLN(str)
+    #define DEBUG_VERBOSE(str)
 #endif
 
 #endif
