e8365498ee75480bc9b59b000a3a9228bab8e0dd
Juan Pedro <JuanP.95.torres@gmail.com>
2018-01-02 22:58:40 +0100

    Added Print w/o warning mode
    Added a mode without printing the verbose deactivated warning.

--- DebugUtils/DebugUtils.h
+++ DebugUtils/DebugUtils.h
@@ -2,18 +2,18 @@
 #define DEBUGUTILS_H
 
 
-#if DEBUG==1 // SIMPLE DEBUG
-    #define DEBUG_PRINT(str)    Serial.print(str);
-    #define DEBUG_PRINTLN(str)  Serial.println(str);
-    #define DEBUG_VERBOSE(str)  \
+#if DEBUG == 2 // PRINT WITH VERBOSE WARNING DEBUG
+#define DEBUG_PRINT(str)    Serial.print(str);
+#define DEBUG_PRINTLN(str)  Serial.println(str);
+#define DEBUG_VERBOSE(str)  \
         Serial.print("Not showing verbose mode in line: "); \
         Serial.print(__LINE__);             \
         Serial.println(" due to not verbose configure.");
 
-#elif DEBUG == 2 // VERBOSE
-    #define DEBUG_PRINT(str)    DEBUG_VERBOSE(str);
-    #define DEBUG_PRINTLN(str)  DEBUG_VERBOSE(str);
-    #define DEBUG_VERBOSE(str)  \
+#elif DEBUG == 3 // VERBOSE DEBUG MODE
+#define DEBUG_PRINT(str)    DEBUG_VERBOSE(str);
+#define DEBUG_PRINTLN(str)  DEBUG_VERBOSE(str);
+#define DEBUG_VERBOSE(str)  \
         Serial.print(millis()); \
         Serial.print(": ");    \
         Serial.print(__PRETTY_FUNCTION__); \
@@ -24,10 +24,16 @@
         Serial.print(' ');          \
         Serial.println(str);
 
+#if DEBUG==1 // SIMPLE PRINT MODE
+#define DEBUG_PRINT(str)    Serial.print(str);
+#define DEBUG_PRINTLN(str)  Serial.println(str);
+#define DEBUG_VERBOSE(str)
+
+
 #elif DEBUG == 0
-    #define DEBUG_PRINT(str)
-    #define DEBUG_PRINTLN(str)
-    #define DEBUG_VERBOSE(str)
+#define DEBUG_PRINT(str)
+#define DEBUG_PRINTLN(str)
+#define DEBUG_VERBOSE(str)
 #endif
 
 #endif
